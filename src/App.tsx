1e7ea972b6d5aa228f901c933b6fbf3d7e1148c0
Vladislav <vladislav.pugachev@t-a9.com>
2026-03-21 14:49:59 +0200

    custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,20 @@
-import { useMemo, useState } from 'react'
-import type { KeyboardEvent } from "react";
-import type { ChangeEvent } from "react";
-
 import './App.scss'
 import TasksList from './components/TasksList';
-import type { ITask } from './types/Base.types';
 import AddTask from './components/AddTask';
 import FilterTasks from './components/FilterTasks';
+import useTasks from './hooks/useTasks';
 
 function App() {
-  const [tasks, setTasks] = useState<ITask[]>([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const filteredTasks = useMemo(() => {
-    return tasks.filter((task: ITask) => task.title.toLowerCase().includes(searchQuery.toLowerCase().trim()));
-  }, [tasks, searchQuery]);
-
-  const addNewTask = (taskName: string) => {
-    if (taskName) {
-      const newTask = {
-        title: taskName,
-        id: Date.now(),
-        isActive: true
-      }
-      setTasks(prev => [...prev, newTask])
-    }
-  }
-
-  const handleEnterKey = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key  === 'Enter') {
-      if (e.currentTarget.value) {
-        addNewTask(e.currentTarget.value);
-      }
-    }
-  }
-
-  const removeTask = (id: number) => {
-    setTasks(tasks => tasks.filter(item => item.id !== id))
-  }
-
-  const editTask = (id: number, title: string) => {
-    setTasks(tasks => tasks.map(item => {
-      return item.id === id ? { ...item, title } : item
-    }));
-  }
-
-  const changeStatusTask = (id: number) => {
-    setTasks(tasks =>
-      tasks.map(item =>
-        item.id === id
-          ? { ...item, isActive: !item.isActive }
-          : item
-      )
-    );
-  }
+  const {
+    filteredTasks,
+    addNewTask,
+    editTask,
+    changeStatusTask,
+    removeTask,
+    searchQuery,
+    setSearchQuery,
+    handleEnterKey,
+  } = useTasks();
 
   return (
     <div className='app-wrapper'>
---
+++ src/hooks/useTasks.tsx
@@ -0,0 +1,64 @@
+import { useMemo, useState } from "react";
+import type { KeyboardEvent } from "react";
+import type { ITask } from "../types/Base.types";
+
+const useTasks = () => {
+    const [tasks, setTasks] = useState<ITask[]>([]);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filteredTasks = useMemo(() => {
+        return tasks.filter((task: ITask) => task.title.toLowerCase().includes(searchQuery.toLowerCase().trim()));
+      }, [tasks, searchQuery]);
+
+      const addNewTask = (taskName: string) => {
+        if (taskName) {
+          const newTask = {
+            title: taskName,
+            id: Date.now(),
+            isActive: true
+          }
+          setTasks(prev => [...prev, newTask])
+        }
+      }
+      
+      const handleEnterKey = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key  === 'Enter') {
+          if (e.currentTarget.value) {
+            addNewTask(e.currentTarget.value);
+          }
+        }
+      }
+      
+      const removeTask = (id: number) => {
+        setTasks(tasks => tasks.filter(item => item.id !== id))
+      }
+      
+      const editTask = (id: number, title: string) => {
+        setTasks(tasks => tasks.map(item => {
+          return item.id === id ? { ...item, title } : item
+        }));
+      }
+      
+      const changeStatusTask = (id: number) => {
+        setTasks(tasks =>
+          tasks.map(item =>
+            item.id === id
+              ? { ...item, isActive: !item.isActive }
+              : item
+          )
+        );
+      }
+
+    return {
+        filteredTasks,
+        addNewTask,
+        editTask,
+        changeStatusTask,
+        removeTask,
+        searchQuery,
+        setSearchQuery,
+        handleEnterKey,
+    };
+}
+
+export default useTasks;
